40711b5a67d4647e7644ecb0fc64c6b3f4f5c10d
qike520 <huangfeihu1995@outlook.com>
2019-06-07 08:44:54 +0800

    Update index.js

--- scratchX/index.js
+++ scratchX/index.js
@@ -29,14 +29,26 @@
        return false;
     };
 
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-            ['', 'run alarm after %n seconds', 'set_alarm', '2'],
-            ['h', 'when alarm goes off', 'when_alarm'],
-        ]
-    };
-
+  var descriptor = {
+    blocks: [
+        ['w', 'turn motor on for %n secs',             'motorOnFor', 1],
+        [' ', 'turn motor on',                         'allMotorsOn'],
+        [' ', 'turn motor off',                        'allMotorsOff'],
+        [' ', 'set motor power %n',                    'startMotorPower', 100],
+        [' ', 'set motor direction %m.motorDirection', 'setMotorDirection', 'this way'],
+        ['h', 'when distance %m.lessMore %n',          'whenDistance', '<', 20],
+        ['h', 'when tilt %m.eNe %n',                   'whenTilt', '=', 1],
+        ['r', 'distance',                              'getDistance'],
+        ['r', 'tilt',                                  'getTilt']
+    ],
+    menus: {
+        motorDirection: ['this way', 'that way', 'reverse'],
+        lessMore: ['<', '>'],
+        eNe: ['=','not =']
+    },
+    url: 'http://info.scratch.mit.edu/WeDo',
+    displayName: 'LEGO WeDo 1.0'
+};
     // Register the extension
     ScratchExtensions.register('Alarm extension', descriptor, ext);
 })({});
