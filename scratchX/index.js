e164c2f19461063379cbda4fbf618ceac538a8ac
qike520 <huangfeihu1995@outlook.com>
2019-06-07 08:06:04 +0800

    Update index.js

--- scratchX/index.js
+++ scratchX/index.js
@@ -1,4 +1,6 @@
 (function(ext) {
+    var alarm_went_off = false; // This becomes true after the alarm goes off
+
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -8,19 +10,31 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.get_temp = function(location, callback) {
-        // Make an AJAX call to the Open Weather Maps API
-         console.log("test data");
-          alert("aa");
+    ext.set_alarm = function(time) {
+       window.setTimeout(function() {
+           alarm_went_off = true;
+       }, time*1000);
+    };
+
+    ext.when_alarm = function() {
+       // Reset alarm_went_off if it is true, and return true
+       // otherwise, return false.
+       if (alarm_went_off === true) {
+           alarm_went_off = false;
+           return true;
+       }
+
+       return false;
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['R', 'current temperature in city %s', 'get_temp', 'Boston, MA'],
+            ['', 'run alarm after %n seconds', 'set_alarm', '2'],
+            ['h', 'when alarm goes off', 'when_alarm'],
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Weather extension', descriptor, ext);
+    ScratchExtensions.register('Alarm extension', descriptor, ext);
 })({});
